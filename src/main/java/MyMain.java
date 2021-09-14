a895a55bbcfcba4083b3caafb3bf4d5c3a325751
savonnahwong <23swong@athenian.org>
2021-09-13 21:11:28 -0700

    Attempted to finish but CalculateE does not work

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,32 +4,49 @@ public class MyMain {
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        int fact = 1;
+        for (int i=1; i<=x; i++) {
+            fact = fact * i;
+        }
+        return fact;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double sum = 0;
+        for (int i=1; i<=1000; i++){   //(sum==Math.E-0.0001 || sum==Math.E+0.0001)
+            double e = (1/factorial(i));
+            sum+=e;
+            if (e == 0){
+                break;
+            }
+        }
+        return sum;
     }
 
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1.0;
+        double n = x;
+        double m = 1;
+        //double div = x/n;     // I dont get what this div does becuase did not work when i tried it.
+        while (n-m > 0.00001){
+            n = (n+m)/2;
+            m = x/n;
+        }
+
+
+        return n;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
+        System.out.println("Enter a positive number:");
+        double inputdouble = scan.nextDouble();
+        System.out.println("The squareroot of " + inputdouble + " is: " + babylonian(inputdouble));
+        System.out.println("The value of e is roughly: " + calculateE());
 
         scan.close();
     }
